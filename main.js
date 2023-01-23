299dbab6108de5533b8fb4d776fa5ca1b8e946b0
iiTzJacob <jj.colon7802@gmail.com>
2023-01-23 10:19:22 -0500

    Delete previous code to restart

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
-const main = document.getElementsByClassName("main")
-const addBook = document.getElementById("add")
-
-
-const Book = {
-    title: String,
-    author: String,
-    pages: 0,
-}
-
-let library = [{title: "Harry Potter", author: "JK Rowling", pages: 300}, {title: "Diary of a Wimpy Kid", author: "test", pages: 217}, {title: "Test", author: "Test", pages: 200}]
+let library = []
 
 function book() {
-    let newDiv = document.createElement("div")
-
-    library.forEach ( book => {
-        newDiv.textContent = book
-    })
+    
 }
 
 function addBookToLibrary() {
